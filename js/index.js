e834147eb14e9f12ad871c285b83bca8a0ed863d
Vasyl Popovych <popovich.w.ppc@gmail.com>
2023-11-06 22:29:06 +0200

    ADD: preload images function to index.js

--- js/index.js
+++ js/index.js
@@ -2,24 +2,54 @@ import { imagePaths } from "./images.js";
 
 const container = document.querySelector(".container");
 
-const changeBackgroundImage = () => {
-    const randomIndex = Math.floor(Math.random() * imagePaths.length);
-    container.style.backgroundImage = `url(${imagePaths[randomIndex]})`;
-    sessionStorage.setItem("lastDisplayedImage", imagePaths[randomIndex]);
+function preloadImage(url) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = reject;
+        img.src = url;
+    });
+}
+
+const preloadImages = async (imagePaths) => {
+    const imageObjects = await Promise.all(imagePaths.map(preloadImage));
+    return imageObjects;
 };
 
-const lastDisplayedImage = sessionStorage.getItem("lastDisplayedImage");
+const applyBackgroundImage = (image) => {
+    container.style.backgroundImage = `url(${image.src})`;
+    sessionStorage.setItem("lastDisplayedImage", image.src);
+};
+
+const setRandomBackgroundImage = async () => {
+    try {
+        const preloadedImages = await preloadImages(imagePaths);
+
+        const lastDisplayedImage = sessionStorage.getItem("lastDisplayedImage");
+        if (lastDisplayedImage) {
+            const cachedImage = preloadedImages.find((image) => image.src === lastDisplayedImage);
+            if (cachedImage) {
+                applyBackgroundImage(cachedImage);
+            } else {
+                applyBackgroundImage(preloadedImages[0]);
+            }
+        } else {
+            applyBackgroundImage(preloadedImages[0]);
+        }
+
+        const rotationInterval = setInterval(() => {
+            const randomIndex = Math.floor(Math.random() * preloadedImages.length);
+            applyBackgroundImage(preloadedImages[randomIndex]);
+        }, 10000);
+
+        setTimeout(() => {
+            clearInterval(rotationInterval);
+        }, 60000);
+    } catch (error) {
+        console.error("Error preloading images:", error);
+    }
+};
 
 setTimeout(() => {
-    if (lastDisplayedImage) {
-        container.style.backgroundImage = `url(${lastDisplayedImage})`;
-    } else {
-        changeBackgroundImage();
-    }
-
-    const rotationInterval = setInterval(changeBackgroundImage, 10000);
-
-    setTimeout(() => {
-        clearInterval(rotationInterval);
-    }, 60000);
+    setRandomBackgroundImage();
 }, 2000);
